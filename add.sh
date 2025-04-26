f7949948348358ec84b9e2d738ddbdb18bbf30c6
Chpokich <goggomon@gmail.com>
2025-04-26 06:07:08 -0800

    v2.1

--- add.sh
+++ add.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #user
-sudo mv i3 /home/user2/.config/
-sudo mv i3status /home/user2/.config/
-sudo mv .vimrc /home/user2/
-sudo mv .vim /home/user2/
-sudo mv coc /home/user2/.config/
-sudo mv .zshrc /home/user2/
-sudo mv .oh-my-zsh /home/user2/
+sudo cp -r i3 /home/user2/.config/
+sudo cp -r i3status /home/user2/.config/
+sudo cp .vimrc /home/user2/
+sudo cp .vim /home/user2/
+sudo cp -r coc /home/user2/.config/
+sudo cp .zshrc /home/user2/
+sudo cp -r .oh-my-zsh /home/user2/
 
 #root
-sudo mv .vimrc /root/
-sudo mv .vim /root/
-sudo mv coc /root/.config/
-sudo mv .zshrc /root/
-sudo mv .oh-my-zsh /root/
+sudo cp .vimrc /root/
+sudo cp .vim /root/
+sudo cp -r coc /root/.config/
+sudo cp .zshrc /root/
+sudo cp -r .oh-my-zsh /root/
