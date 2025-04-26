1c40daaae938797e83d774fe9e06b8c01c11e7d5
Chpokich <goggomon@gmail.com>
2025-04-26 06:14:14 -0800

    v2.2

--- add.sh
+++ add.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+
+echo "input a system user name:"
+read user
+sudo su
+
 #user
-sudo cp -r i3 /home/user2/.config/
-sudo cp -r i3status /home/user2/.config/
-sudo cp .vimrc /home/user2/
-sudo cp .vim /home/user2/
-sudo cp -r coc /home/user2/.config/
-sudo cp .zshrc /home/user2/
-sudo cp -r .oh-my-zsh /home/user2/
+sudo cp -r i3 /home/$user/.config/
+sudo cp -r i3status /home/$user/.config/
+sudo cp .vimrc /home/$user/
+sudo cp .vim /home/$user/
+sudo cp -r coc /home/$user/.config/
+sudo cp .zshrc /home/$user/
+sudo cp -r .oh-my-zsh /home/$user/
 
 #root
 sudo cp .vimrc /root/
