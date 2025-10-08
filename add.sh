f90720ea787a991953b76086bada65bb4206f459
Chpokich <goggomon@gmail.com>
2025-10-08 09:45:02 +0300

    add.sh ready version

--- add.sh
+++ add.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 
-
-echo "input a system user name:"
-read user
 sudo su
+user=$(whoami)
 
-#user
+sudo rm -rf /home/$user/.config/i3
 sudo cp -r i3 /home/$user/.config/
+
+sudo rm -rf /home/$user/.config/i3status
 sudo cp -r i3status /home/$user/.config/
+
+sudo rm -rf /home/$user/.vimrc
 sudo cp .vimrc /home/$user/
+
+sudo rm -rf /home/$user/.vim
 sudo cp .vim /home/$user/
+
+sudo rm -rf /home/$user/.config/coc
 sudo cp -r coc /home/$user/.config/
+
+sudo rm -rf /home/$user/.zshrc
 sudo cp .zshrc /home/$user/
+
+sudo rm -rf /home/$user/.oh-my-zsh
 sudo cp -r .oh-my-zsh /home/$user/
 
+
 #root
+sudo rm -rf /root/.vimrc
 sudo cp .vimrc /root/
+
+sudo rm -rf /root/.vim
 sudo cp .vim /root/
+
+sudo rm -rf /root/.config/coc
 sudo cp -r coc /root/.config/
+
+sudo rm -rf /root/.zshrc
 sudo cp .zshrc /root/
+
+sudo rm -rf /root/.oh-my-zsh
 sudo cp -r .oh-my-zsh /root/
+
